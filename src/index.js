e26b37d67fa4a44871c6926c3c8b9fd6f219dcb9
Pablo Borjas <pabloborjas_0708@hotmail.com>
2021-08-25 18:11:51 +0200

    ya logre hacer que se viera todo en la pagina falta los estilos

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/*  Your code goes here */}
-  </React.StrictMode>,
-  document.getElementById('root')
+const element = (
+  <div className='home'>
+    <h1>Say Hello to ReactJS</h1>
+    <p>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+    <div className='second'>
+  <img
+      src='/images/icon1.png'
+      alt='A kid using a keyboard as a surf board'
+    />
+  </div>
+  <div className='second'>
+  <img
+      src='/images/icon2.png'
+      alt='A kid using a keyboard as a surf board'
+    />
+  </div>
+
+
+  </div>
+ 
 );
+
+
+
+ReactDOM.render(element, document.getElementById('root'));
