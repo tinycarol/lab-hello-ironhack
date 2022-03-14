dd56fbd4e3ad893f294231ece083cd93e06c3f3e
Eduardo Francos Margareto <efmargareto@hotmail.com>
2022-03-14 21:38:40 +0100

    nav components + app.js component + hero

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import Hero from './Hero';
+import Nav from './Nav'
+
+
+function App() {
+    return (
+        <>
+            <Nav />
+            <Hero />
+        </>
+    )
+}
+
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+const Hero = () => {
+    return (
+
+        <div id="hero">
+            <section>
+                <h1>Says Hello to <br/>to ReactJS</h1>
+                <p className='text'>You will learn how to use the most popular frontend library, and become a super Ninja developer</p>
+                <button>Awesome</button>
+            </section>
+        </div>
+    )
+}
+
+export default Hero
