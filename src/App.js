467dbf4d311da78e7b739edcba9fbc94e65915df
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-03-14 21:35:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar/Navbar.js';
+import Hero from './components/Hero/Hero.js'
+import './App.css';
+
+function App() {
+    return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Hero />
+        </header>
+        <body>
+            <div>
+                
+            </div>
+        </body>
+      </div>
+    </>
+    );
+  }
+  
+  export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+      <navbar className="custom-navbar">
+        <img src="../images/ironhack-logo.svg" alt="" />
+        <img src="../images/menu-top.svg" alt="" />
+      </navbar>
+    );
+  }
+  
+  export default Navbar;
