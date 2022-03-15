c636790fce15bf4a6a7789216cac3302c62e8e00
Isa Gutierrez <isagr_kio@hotmail.com>
2022-03-15 13:41:15 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import './App.css'
 
+const features = [
+    {
+        title: 'Declarative',
+        img: './images/icon1.png',
+        description: 'React makes it painless to create interactive UIs.'
+    },
+    {
+        title: 'Components',
+        img: './images/icon2.png',
+        description: 'Build encapsulated components that manage their state.'
+    },
+    {
+        title: 'Single-Way',
+        img: './images/icon3.png',
+        description: 'A set of immutable values are passed to the components.'
+    },
+    {
+        title: 'JSX',
+        img: './images/icon4.png',
+        description: 'Statically-typed, designed to run on modern browsers.'
+    }
+]
+
 function App() {
     return(
         <div>
-            <nav>
-                <img src="../images/ironhack-logo.svg" alt="Ironhack logo" className='logo'/>
-                <img src="../images/menu-top.svg" alt="Menu top" className='hamburger'/>
-            </nav>
 
             <div className="hero"> 
                 <div>
                     <h1>Say hello to ReactJS</h1>
                     <p>You will learn how to use the most popular fronted library, and become a super Ninja developer.</p>
-                    <button>Awesome</button>
+                    <button>Awesome!</button>
                 </div>
 
+                <img src="../images/react-logo.svg" alt="React logo" />
+            </div>
+
+            <div className="features">
+                {features.map(feature => 
                 <div>
-                    <img src="../images/react-logo.svg" alt="React logo" />
-                </div>
-
+                    <img src={feature.img} alt="Feature icon" className="features-icons" />
+                    <h3>{feature.title}</h3>
+                    <p>{feature.description}</p>
+                </div>)}
+            
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar.js'
 
 ReactDOM.render(
   <React.StrictMode>
+    <>
+    <Navbar />
     <App />
+    </>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
