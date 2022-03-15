2ac882f312ec27893dd21351d5a8742326a66334
David <davidblancoferrandez@gmail.com>
2022-03-15 11:20:08 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import { App } from './App/App'
+
 ReactDOM.render(
   <React.StrictMode>
-    {/*  Your code goes here */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
