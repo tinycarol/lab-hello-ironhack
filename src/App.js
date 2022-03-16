d39036e3a44d8234511c7097448a4fd4e1d66517
Pablo Mateos <pmateos805@gmail.com>
2022-03-16 02:19:23 +0100

    features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Hero from './components/hero/Hero.js'
+import Card from './components/card/Card.js'
 
 function App() {
     return (
         <>
             <Hero />
+            <Card />
         </>
     )
 }
 
-export default App
+export default App 
