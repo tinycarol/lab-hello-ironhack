0f4aeddcdae61526bce00e9df27110856deae8a0
mblason <marina.blasongraviz@gmail.com>
2022-09-17 14:22:13 +0200

    lab done

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import "./Button.css";
+
+const Button = ({ text }) => {
+    return (
+        <button className="btn">
+            {text}
+        </button>
+    );
+};
+
+export default Button;
