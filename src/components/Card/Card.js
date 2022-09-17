bd8b47f100fe70f58eb6fc5e99acdb9610282320
violeta atkinson <violeta.atkinson@gmail.com>
2022-09-17 14:48:50 +0200

    done

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import './Card.css';
+
+const Card = ({ title, text, img }) => {
+    return(
+        <div className='Card'>
+           <img src={img} alt="img"/>
+           <h4>{title}</h4>
+           <p>{text}</p>
+        </div>
+    )
+}
+  
+  export default Card
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return(
+        <div className='Navbar'>
+            <img src="images/ironhack-logo.svg"  className='Navbar-img'alt="logo"/>
+            <img src="images/menu-top.svg" alt="logo"/>
+        </div>
+    )
+}
+  
+  export default Navbar
