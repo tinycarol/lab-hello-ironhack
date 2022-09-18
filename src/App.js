4e49f100bcd0fbab0cb24aae1a4ebeca5f514c69
violeta atkinson <violeta.atkinson@gmail.com>
2022-09-18 19:11:42 +0200

    css done

--- src/App.js
+++ src/App.js
@@ -2,46 +2,42 @@ import React from "react";
 import "./App.css";
 import Card from "./components/Card/Card";
 import Hero from "./components/Hero/Hero";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
 
-
-
-const App = () => {  
-    
-    const cards = [
-        {
-            img: "/images/icon1.png",
-            title: 'Declarative',
-            text: "React makes it painless to create interactive UIs."
-        },
-        {
-            img:"/images/icon2.png",
-            title:"Components",
-            text:"Build encapsulated components that manage their state."
-
-        },
-        {
-            img:"/images/icon3.png",
-            title:"Single-Way",
-            text:"A set of inmmutable values are pased to the component's."
-        },
-        {
-            img:"/images/icon4.png",
-            title:"JSX",
-            text:"Static-typed, designed to run on modern browsers"
-        }
-    ]
-   return (
-        <div className="App">
-            <Navbar/>
-            <Hero/>
-            {cards.map(({img,title,text}) => {
-                return <Card img={img} title={title} text={text} key={img} />
-            } )}
-        </div>
-    )
-   
-}
-
+const App = () => {
+	const cards = [
+		{
+			img: "/images/icon1.png",
+			title: "Declarative",
+			text: "React makes it painless to create interactive UIs.",
+		},
+		{
+			img: "/images/icon2.png",
+			title: "Components",
+			text: "Build encapsulated components that manage their state.",
+		},
+		{
+			img: "/images/icon3.png",
+			title: "Single-Way",
+			text: "A set of inmmutable values are pased to the component's.",
+		},
+		{
+			img: "/images/icon4.png",
+			title: "JSX",
+			text: "Static-typed, designed to run on modern browsers",
+		},
+	];
+	return (
+		<div className="App">
+			<Navbar />
+			<Hero />
+			<div className="Container">
+				{cards.map(({ img, title, text }) => {
+					return <Card img={img} title={title} text={text} key={img} />;
+				})}
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 const Navbar = () => {
     return(
         <div className='Navbar'>
-            <img src="images/ironhack-logo.svg"  className='Navbar-img'alt="logo"/>
+            <img src="images/ironhack-logo.svg" alt="logo"/>
             <img src="images/menu-top.svg" alt="logo"/>
         </div>
     )
