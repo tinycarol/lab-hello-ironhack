610c5da6950ab156382656446c7cfcd58266cb7d
hilkizun <hilkizun@gmail.com>
2023-03-03 16:37:50 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import './App.css';
 
+function Card(props) {
+  return (
+    <div className='card-container'>
+        {props.elem?.map((elem) => {
+          return (
+          <div key={elem.title} className="elem-card">
+            <img src={elem.image} alt={elem.description} />
+            <h2>{elem.title}</h2>
+            <p>{elem.description}</p>
+          </div>
+          )
+        })}
+    </div>
+  )
+}
+
+
+
 function App () {
   return (
     <div className='App'>
@@ -11,11 +29,31 @@ function App () {
         <div className='hero-content'>
           <h1>Say hello to<br></br>ReactJS</h1>
           <p>You will learn how to use<br></br>the most popular fronted library,<br></br>and become a super Ninja developer.</p>
-          <button>Awesome!</button>
+          <a href="https://my.ironhack.com/" target="_blank" rel="noopener noreferrer"><button>Awesome!</button></a>
         </div>
       </div>
-      <div className="cards">
-      </div>
+      <Card elem = {[
+        {
+          title: "Declarative",
+          description: "React makes it painless to create interactive UIs.",
+          image: "/images/icon1.png",
+        },
+        {
+          title: "Components",
+          description: "Build encapsulated components that manage their state.",
+          image: "/images/icon2.png",
+        },
+        {
+          title: "Single-Way",
+          description: "A set of immutable values are passed to the component's.",
+          image: "/images/icon3.png",
+        },
+        {
+          title: "JSX",
+          description: "Statically-typed, designed to run on modern browsers.",
+          image: "/images/icon4.png",
+        },
+      ]}/>
     </div>
   );
 }
